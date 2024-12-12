4a3f2123f44b40f907861ffea2a0ec18aa62584f
jyoti saxena <j21saxena@gmail.com>
2024-12-12 10:48:47 +0530

    Create FirebaseMessagingService.java

---
+++ FirebaseMessagingService.java
@@ -0,0 +1,10 @@
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        // Message handling is done here
+    }
+}
